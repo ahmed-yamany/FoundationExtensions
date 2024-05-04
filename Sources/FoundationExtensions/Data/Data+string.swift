b68b238bbfdc8c21d15a9a673abef91894c8047c
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-05-04 13:40:55 +0300

    data encode and decode

--- Sources/FoundationExtensions/Data/Data+string.swift
+++ Sources/FoundationExtensions/Data/Data+string.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  Data+string.swift
+//
 //
 //  Created by Ahmed Yamany on 29/04/2024.
 //
 
 import Foundation
+
+public extension Data {
+    func string() -> String? {
+        String(data: self, encoding: .utf8)
+    }
+}
