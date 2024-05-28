9372ece0b8de0d54b6310ae1b981dfb5aa7e5ecc
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-05-28 23:35:21 +0300

    safe. optional wrapping using compact map

--- Sources/FoundationExtensions/Collection/Array+to.swift
+++ Sources/FoundationExtensions/Collection/Array+to.swift
@@ -9,31 +9,20 @@ import Foundation
 
 public extension Array where Element == Data {
     func  toURL() -> [URL] {
-        var urls: [URL] = []
-        for case let .some(url) in  map({ URL(dataRepresentation: $0, relativeTo: nil) }) {
-            urls.append(url)
-        }
-        return urls
+        map({ URL(dataRepresentation: $0, relativeTo: nil) }).compactMap { $0 }
     }
 }
 
 public extension Array where Element == URL {
     func toData() -> [Data] {
-        let allData: [Data?] =  map({
+        let data: [Data?] =  map({
             guard $0.startAccessingSecurityScopedResource() else {
                 debugPrint("Error: could not access content of url: \($0)")
                 return nil
             }
             return try? Data(contentsOf: $0)
         })
-        
-        var data: [Data] = []
-        
-        for case let .some(i) in allData {
-            data.append(i)
-        }
-        
-        return data
+        return data.compactMap { $0 }
     }
 }
 
--- Sources/FoundationExtensions/Collection/NSSet+to.swift
+++ Sources/FoundationExtensions/Collection/NSSet+to.swift
@@ -9,10 +9,6 @@ import Foundation
 
 public extension NSSet {
     func toArray<T>() -> [T] {
-        var array: [T] = []
-        for case let .some(i) in map({ $0 as? T }) {
-            array.append(i)
-        }
-        return array
+        map({ $0 as? T }).compactMap { $0 }
     }
 }
